d9097348ded1a4d29d402233d6722c880a93cfc8
caetanopf <caetanopfreitas@gmail.com>
2024-02-28 11:25:14 -0300

    novo

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script> 
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  methods: {
+    incrementar() {
+      this.contador++;
+    },
+    decrementar() {
+      if (this.contador > 0) {
+        this.contador--;
+      }
+    },
+    reiniciar() {
+      this.contador = 0;
+    },
+  },
+};
+</script>
+
+<template>
+<div>
+  <h1>Contador</h1>
+  <p> {{ contador }} </p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Decrementar</button>
+  <button @click="reiniciar">Reiniciar</button>
+</div>
+</template>
